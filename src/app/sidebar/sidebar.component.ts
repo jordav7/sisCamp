62f6b9711610fa9bf86efdba62ebe666624a9a77
David Cruz <jordav_7@hotmail.com>
2017-02-15 23:42:06 -0500

    Se aumenta el menu dinamico desde una variable

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,15 +11,37 @@ export class SidebarComponent implements OnInit {
   menus: any[];
   CURRENT_USER: any = JSON.parse(localStorage.getItem('currentUser'));
   constructor() {
-    this.menus = [{title: 'Seguridades',
-      route: '',
-      childrens: [
-        {title: 'Registro Ente', route: 'ente', childrens: []},
-        {title: 'Registro de Usuarios', route: 'usuario', childrens: []},
-        {title: 'Gestion de Roles', route: 'roles', childrens: []},
-        {title: 'Configuracion Menus', route: '#', childrens: []},
-        {title: 'Parametros Generales', route: 'parametro', childrens: []}
-      ]}];
+    this.menus = [
+      {
+        title: 'Seguridades',
+        route: '',
+        childrens: [
+          {title: 'Registro Ente', route: 'ente', childrens: []},
+          {title: 'Registro de Usuarios', route: 'usuario', childrens: []},
+          {title: 'Gesti\u00f3n de Roles', route: 'roles', childrens: []},
+          {title: 'Configuraci\u00f3n Men\u00fas', route: '#', childrens: []},
+          {title: 'Par\u00e1metros Generales', route: 'parametro', childrens: []}
+        ]
+      },
+      {
+        title: 'Administraci\u00f3n',
+        route: '',
+        childrens: [
+          {title: 'Cat\u00e1logos', route: 'catalogo', childrens: []},
+          {title: 'Registro Ligas', route: 'ligas', childrens: []},
+          {title: 'Registro Jugadores', route: 'jugador', childrens: []},
+          {title: 'Registro \u00c1rbitro', route: 'arbitro', childrens: []},
+          {title: 'Ubicaci\u00f3n Geogr\u00e1fica', route: 'ubicacionGeografica', childrens: []}
+        ]
+      },
+      {
+        title: 'Procesos',
+        route: '',
+        childrens: [
+          {title: 'Registro Equipos', route: 'equipos', childrens: []}
+        ]
+      }
+    ];
   }
 
   ngOnInit() {
